4e7cbe25a8ca375e2023440477fdd2a8696b7b74
BigSwierku <winiarski.sz@gmail.com>
2018-05-21 14:07:09 +0200

    First JUnit test. Created and set pom.xml

---
+++ qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
@@ -0,0 +1,16 @@
+package com.jsystems.testautomation;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+public class JunitTest {
+
+
+    @Test
+    public void firstTest(){
+        assertTrue(true);
+    }
+
+}
